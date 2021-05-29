0ce42a6ad0169a505e6b31adc3c3e8d121d271eb
ktenzer <keith.tenzer@gmail.com>
2021-05-29 12:38:54 -0700

    added sandstone peak hike

--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -43,7 +43,7 @@ var documents = [{
     "id": 8,
     "url": "http://localhost:4000/sandstone-peak/",
     "title": "Sandstone Peak Hike",
-    "body": "2021/04/12 -  1234Activity: HikingDuration: 3 hoursDifficulty: EasyBring: Hiking boots, towel, extra socks, water and snacks"
+    "body": "2021/04/12 -  Sandstone peak is the highest mountain in the santa monica mountains at 3,111 ft. The trailhead starts around 2,000 ft. It is about a 1,111 ft up elevation gain. The trail is an out and back but you can make a longer loop to extend your tour. The hike has very limited shade so it isn’t recommended when it is too hot. The trail will ascend very quickly. The first part is the most challenging but then it gets more gradual. Enjoy some breathtaking views of the santa monica mountains and rugged costaline. About 30 minutes into the trail to your right is Malibu lake which is a very secluded area off mullholand hwy and home to many famous people that are low key. On a clear day you will get a great view of catalina island jolting out of the pacific. The last part of the hike is a single track scamper up to the peak. If you want to go to highest point some bouldering is required, nothing crazy but be careful. The views at the highest perch in the santa monica mountains are breathtaking. You feel like an eagle on top of the world, soaring. 1234Activity: HikingDuration: 3 hoursDifficulty: ModerateBring: Hiking boots, water and snacks"
     }];
 
 var idx = lunr(function () {
